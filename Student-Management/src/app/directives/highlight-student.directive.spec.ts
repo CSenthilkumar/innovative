c159f7d32c840d04c4bf6dd7ecabd25b4ac455b4
Gutz <senthilk5050@gmail.com>
2023-10-21 22:50:07 +0530

    Initial Version
    Added Initial version

---
+++ Student-Management/src/app/directives/highlight-student.directive.spec.ts
@@ -0,0 +1,16 @@
+/**
+ * Created By : Senthilkumar
+ */
+
+import { HighlightStudentDirective } from './highlight-student.directive';
+
+describe('HighlightStudentDirective', () => {
+  it('should create an instance', () => {
+    const directive: any = new HighlightStudentDirective();
+    expect(directive).toBeTruthy();
+  });
+});
+
+/**
+ * Created By : Senthilkumar
+ */
